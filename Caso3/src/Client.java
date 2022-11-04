694a4c8f90e023de712a7a05b0e87511d2993ab2
Juan Camilo Reyes <j.reyesc@uniandes.edu.co>
2022-11-04 17:37:21 -0500

    finished steps 6a to 13

--- Caso3/src/Client.java
+++ Caso3/src/Client.java
@@ -9,6 +9,9 @@ import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.util.Scanner;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
 public class Client {
 
     SecurityFunctions f;
@@ -36,10 +39,6 @@ public class Client {
             BigInteger g = new BigInteger(dc.readLine());
             BigInteger p = new BigInteger(dc.readLine());
             BigInteger commonVal = new BigInteger(dc.readLine());
-            System.out.println("Received Diffie-Hellman data: ");
-            System.out.println("g: " + g);
-            System.out.println("p: " + p);
-            System.out.println("commonVal: " + commonVal);
             
             
 
@@ -48,17 +47,11 @@ public class Client {
 			int x = Math.abs(r.nextInt());
     		Long longx = Long.valueOf(x);
     		BigInteger bix = BigInteger.valueOf(longx);
+            //6a.
             BigInteger myVal = G2X(g, bix, p);
-            
-            //Get dh
-            BigInteger dh = G2X(commonVal, bix, p);
-
             //4.
             //get F(K+,m) 
-            //Esto me desencripta algo que no es (deberia funcionar(hay otro test en el que manda error a proposito))
-            //Sospecho que es algo que tiene que ver con el byte[]
             String gPGxMsgSigned = dc.readLine();
-            System.out.println("\nReceived signature: " + gPGxMsgSigned);
             try {
                 boolean check = f.checkSignature(
                     publicaServidor,
@@ -66,27 +59,78 @@ public class Client {
                     // expected message
                     g + "," + p + "," + commonVal
                 );
+                //5.
                 ac.println(check ? "OK" : "ERROR");
             } catch (Exception e) {
                 // TODO should we be sending this when the signature check throws an exception?
                 //      or should we only do it when the signature check actually returns false?
+
+                // JC:I think this is fine as if it throws an exception it means the signature is invalid and it should not continue
                 ac.println("ERROR");
             }
             
+            //6b.
             //Send value
             ac.println(myVal.toString());
             
+            //7a.
+            //Get master key
+            BigInteger masterKey = G2X(commonVal, bix, p);
+            String strMasterKey = masterKey.toString();
 
+            try {
+                //Get symetric keys
+                SecretKey sk_clt = f.csk1(strMasterKey);
+                SecretKey sk_mac = f.csk2(strMasterKey);
+                //Get vector
+                byte[] iv1 = generateIvBytes();
+                IvParameterSpec ivSpec1 = new IvParameterSpec(iv1);
+                //Aks for consultation
+                // TODO: I think this need to be automated for the tests but im not sure 
+                System.out.println("\nEnter the number of the file you want to consult: ");
+                String consulta = scanner.nextLine();
+                //Encrypt
+                byte[] encMsg = f.senc(consulta.getBytes("UTF-8"), sk_clt, ivSpec1, "Cliente");
+                //hmac
+                byte[] macMsg = f.hmac(consulta.getBytes("UTF-8"), sk_mac);
+                //8.
+                //Send
+                ac.println(byte2str(encMsg));
+                ac.println(byte2str(macMsg));
+                ac.println(byte2str(iv1));
 
+                //10.
+                String confirmation = dc.readLine();
+                //It never generates the error so this is never used but I did it bc why not
+                if(confirmation.equals("ERROR")) System.exit(1);
+        //11.
+        //Get data from server
+        String encMsg2 = dc.readLine();
+        String macMsg2 = dc.readLine();
+        String iv2 = dc.readLine();
+        byte[] encMsgBytes = str2byte(encMsg2);
+        byte[] macMsgBytes = str2byte(macMsg2);
+        
+        byte[] ivBytes = str2byte(iv2);
+        IvParameterSpec ivSpec2 = new IvParameterSpec(ivBytes);    
+        //Decrypt
+        byte[] decMsg = f.sdec(encMsgBytes, sk_clt, ivSpec2);
+        //12.
+        //Check hmac
+        boolean verify  = f.checkInt(decMsg, sk_mac, macMsgBytes);
+        //13.
+        ac.println((verify)?"OK":"ERROR");
+        } catch (Exception e) {
+            e.printStackTrace();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+}
 
-    }
-
-    public static void main(String[] args) throws Exception {
-        //Server connection
-        String ip = "127.0.0.1"; //Localhost
+public static void main(String[] args) throws Exception {
+    //Server connection
+    String ip = "127.0.0.1"; //Localhost
         Socket socket = null; 
         try {
             //Create socket
@@ -122,6 +166,12 @@ public class Client {
 			ret += (g.length()==1?"0":"") + g;
 		}
 		return ret;
+	}
+
+    private byte[] generateIvBytes() {
+	    byte[] iv = new byte[16];
+	    new SecureRandom().nextBytes(iv);
+	    return iv;
 	}
 
 }
